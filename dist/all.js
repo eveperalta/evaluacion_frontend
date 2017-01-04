bdf861533f89b6ecac43da77c5209f8805935327
EvePeralta <eperalta.cm@gmail.com>
2017-01-04 20:28:38 -0300

    destinos listos

--- dist/all.js
+++ dist/all.js
@@ -82,8 +82,17 @@ function get_regiones(){
 }
 $(document).ready(function(){
 	// tu codigo va acá
+	 $('[data-toggle="tooltip"]').tooltip(); 
 });
 
+$(document).ready(function() {
+ var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(-33.4375545, -70.65049),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+});
 function Moto(){
 	this.litrosGastados = function(distancia){
 		// retorna la cantidad de litros que gasta en una distancia
--- src/js/custom.js
+++ src/js/custom.js
@@ -2,3 +2,12 @@ $(document).ready(function(){
 	// tu codigo va acá
 	 $('[data-toggle="tooltip"]').tooltip(); 
 });
+
+$(document).ready(function() {
+ var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(-33.4375545, -70.65049),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+});
